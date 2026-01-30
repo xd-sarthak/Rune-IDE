306fc468ac5a7be35783777a5d5447ab1256bf09
xd-sarthak <sarthak.sri0203@gmail.com>
2026-01-30 12:32:15 +0530

    Sentry logging added

---
+++ sentry.server.config.ts
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://097268eae01e220ce096a40d1eb078e8@o4510618913144832.ingest.us.sentry.io/4510798011236352",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+  integrations: [Sentry.consoleLoggingIntegration({levels: ['error', 'warn', 'info','log']})]
+});
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -56,6 +56,11 @@ export const demoGenerateGoogle = inngest.createFunction(
       return await generateText({
         model: google('gemini-2.5-flash'),
         prompt: finalPrompt,
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        }
       }); 
     });
   },
